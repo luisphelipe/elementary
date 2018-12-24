0f3422bfcd4df52c28d1b99f0892d49238a80935
Luis Phelipe <luisphelipecode@gmail.com>
2018-12-24 21:34:15 +0000

    Optimze ruby implementation
    Change program to use simple rulestring instead of bloated hash with
    booleans.

--- elementary.rb
+++ elementary.rb
@@ -1,112 +1,65 @@
+# CONTROLERS
+RULE          = 90
+$ARRAY_LENGTH = 164
+$SLEEP_TIME   = 0.05
+$DOT          = '■'
+$SPACE        = ' '
+
+
+# unecessary to_bin implementarion :^)
 def to_binary_string(int)
     binary = 0
     index = 1
 
     while int > 0 do
-        binary = binary + (int % 2) * index 
-        index  = index * 10
-        int    = int / 2
+        binary += (int % 2) * index 
+        index  *= 10
+        int    /= 2
     end
 
     "%08d" % binary.to_s
 end
 
-# Some interesting rulestrings
-RULE    = 110
-rule22  = '00010110'
-rule30  = '00011110'
-
-# CONTROLERS
-$CHOOSEN_RULE = to_binary_string(RULE)
-# $CHOOSEN_RULE = rule22  
-$ARRAY_LENGTH = 164
-$SLEEP_TIME = 0.07
-$DOT = '■'
-$SPACE = ' '
-
-# Expanded structure for verifying rule
-$BASE_SET = {
-    [true  ,true  ,true ] => nil, 
-    [true  ,true  ,false] => nil, 
-    [true  ,false ,true ] => nil, 
-    [true  ,false ,false] => nil, 
-    [false ,true  ,true ] => nil,
-    [false ,true  ,false] => nil,
-    [false ,false ,true ] => nil,
-    [false ,false ,false] => nil
-}
-
-def parse_rule(rule, base)
-    rule_set = base.clone()
-
-    rule_set.each_with_index { |(key, value), index| 
-        if rule[index] == '1'
-            rule_set[key] = true
-        else
-            rule_set[key] = false    
-        end
-    } 
-
-    rule_set
-end
-
-## This was how I implemented in the beggining
-# def compute_rule_30(array)
-#     array.each_with_index.map { |value, index| 
-#         # Below is the rule30 formula
-#         if index == array.length-1
-#             array[index-1] ^ (value | array[0])
-#         else
-#             array[index-1] ^ (value | array[index+1])
-#         end
-#     }
-# end
-
-# Then i tried to make it run any rule, given a ruleset
-def compute_with_ruleset(array, ruleset)
+# compute current cell state
+def compute_with_rulestring(array, rulestring)
     array.each_with_index.map { |value, index| 
-        if index == array.length-1
-            cell_state = [array[index-1], value, array[0]]
-        else
-            cell_state = [array[index-1], value, array[index+1]]
-        end
+        # left and current cell values, converted from binary
+        cell_state = (1 - array[index-1]) * 4 + (1 - value) * 2
 
-        ruleset[cell_state]        
+        # right cell value, thernary check for end of array
+        cell_state += 1 - (index == array.length() - 1 ? array[0] : array[index+1])
+
+        # returns current cell state, based on choosen rulestring
+        rulestring[cell_state].to_i        
     }
 end
 
+# parse array into printable string
 def parse_string(array)
     array = array.map { |item|
-        item ? $DOT : $SPACE
+        item == 1 ? $DOT : $SPACE
     }.join('')
 end
 
 def main
-    # Generate initial array with random booleans
-    array = Array.new($ARRAY_LENGTH);
-    array.map! { 
-        if Random.rand(2) == 1
-            true
-        else
-            false
-        end
-    }
+    # Generate initial array
+    array = Array.new($ARRAY_LENGTH, 0);
 
-    rule_set = parse_rule $CHOOSEN_RULE, $BASE_SET
+    ## Random initial state
+    # array.map! { 
+    #     Random.rand(2)
+    # }    
+    
+    # Some rules require specific initial state, below is for sierpinski (rule 90)
+    array[$ARRAY_LENGTH/2 - 1] = 1
 
-    # compute_with_ruleset
+    choosen_rule = to_binary_string(RULE)
+
     while true do
         puts parse_string array
-        array = compute_with_ruleset array, rule_set
+        array = compute_with_rulestring array, choosen_rule
         sleep($SLEEP_TIME)
     end
-
-    ## compute_rule_30, uncoment the method for it to work
-    # while true do
-    #     puts parse_string array
-    #     array = compute_rule_30 array
-    #     sleep(SLEEP_TIME)
-    # end
 end
 
 main
